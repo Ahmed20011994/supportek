27e339b7f48ddd400ab6ae355960d1decf7d0a97
Ahmed Ali <ahmed.ali@unikrew.com>
2024-05-26 19:42:59 +0500

    how about now you dumb fuck

--- custom_googletrans/client.py
+++ custom_googletrans/client.py
@@ -14,7 +14,7 @@ from httpx import Timeout
 
 from googletrans import urls, utils
 from googletrans.gtoken import TokenAcquirer
-from constants import (
+from custom_googletrans.constants import (
     DEFAULT_CLIENT_SERVICE_URLS,
     DEFAULT_USER_AGENT, LANGCODES, LANGUAGES, SPECIAL_CASES,
     DEFAULT_RAISE_EXCEPTION, DUMMY_DATA
